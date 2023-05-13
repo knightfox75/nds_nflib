7a22d51d4dffd94d6ec438b1d5d253df68d515d6
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-05-13 13:03:05 +0100

    examples: Add PNG files and conversion scripts for more examples

---
+++ examples/demo/reveal/assets/convert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+grit bola.png -ftb -fh! -gTFF00FF -gt -gB8 -m!
+
+for file in *.bin; do
+    mv -- "$file" "${file%.bin}"
+done
+
+mv *.pal *.img ../nitrofiles/spr
+
+grit nfl.png -ftb -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
+
+for file in *.bin; do
+    mv -- "$file" "${file%.bin}"
+done
+
+mv *.pal *.img *.map ../nitrofiles/bg
---
+++ examples/demo/water/assets/convert.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for file in *.png; do
+    grit "$file" -ftb -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
+done
+
+for file in *.bin; do
+    mv -- "$file" "${file%.bin}"
+done
+
+mv *.pal *.img *.map ../nitrofiles/bg
