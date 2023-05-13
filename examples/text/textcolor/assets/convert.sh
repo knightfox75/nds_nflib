4f90a74d4ae805399c5af444542ea8d0d584c2a4
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-05-13 17:20:19 +0100

    examples: Add assets and scripts to sound and text examples

---
+++ examples/text/textcolor/assets/convert.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+grit nfl.png -ftb -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
+
+for file in *.bin; do
+    mv -- "$file" "${file%.bin}"
+done
+
+mv *.pal *.img *.map ../nitrofiles/bg
+
+grit default.png -ftb -fh! -gTFF00FF -gt -gB8 -m!
+
+for file in *.bin; do
+    mv -- "$file" "${file%.bin}"
+done
+
+for file in *.img; do
+    mv -- "$file" "${file%.img}".fnt
+done
+
+mv *.pal *.fnt ../nitrofiles/fnt
