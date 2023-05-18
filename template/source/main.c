37e1d5cb5692b6a18b3f1a5d442b6860d894f971
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-05-18 02:19:14 +0100

    template: Cleanup and translate comments to English

--- template/source/main.c
+++ template/source/main.c
@@ -1,65 +1,29 @@
 // SPDX-License-Identifier: CC0-1.0
 //
 // SPDX-FileContributor: NightFox & Co., 2009-2011
+//
+// NightFox's Lib Template
+// http://www.nightfoxandco.com
+// Inicio 10 de Octubre del 2009
 
-/*
--------------------------------------------------
-
-	NightFox's Lib Template
-
-	Requiere DevkitARM
-	Requiere NightFox's Lib
-
-	Codigo por NightFox
-	http://www.nightfoxandco.com
-	Inicio 10 de Octubre del 2009
-
--------------------------------------------------
-*/
-
-
-
-
-
-/*
--------------------------------------------------
-	Includes
--------------------------------------------------
-*/
-
-// Includes C
 #include <stdio.h>
 
-// Includes propietarios NDS
 #include <nds.h>
 
-// Includes librerias propias
 #include <nf_lib.h>
 
+int main(int argc, char **argv)
+{
+    consoleDemoInit(); // Initialize console
 
+    printf("\n Hello World!");
 
+    while (1)
+    {
+        swiWaitForVBlank(); // Wait for the screen refresh
+    }
 
-
-/*
--------------------------------------------------
-	Main() - Bloque general del programa
--------------------------------------------------
-*/
-
-int main(int argc, char **argv) {
-
-	consoleDemoInit();		// Inicializa la consola de texto
-	consoleClear();			// Borra la pantalla
-	setBrightness(3, 0);	// Restaura el brillo
-
-	printf("\n Hello World!");
-
-	// Bucle (repite para siempre)
-	while (1) {
-
-		swiWaitForVBlank();		// Espera al sincronismo vertical
-
-	}
-
-	return 0;
+    // If this is reached, the program will return to the loader if the loader
+    // supports it.
+    return 0;
 }
